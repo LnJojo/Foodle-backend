ecaedc0f8cc610bfea8eafd5c641e78c68032353
LnJojo <lnjonathan5@gmail.com>
2025-04-10 11:26:31 +0200

    modified url api/

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("/admin/", admin.site.urls),
-    path('/api/', include('api.urls')),
+    path("admin/", admin.site.urls),
+    path('api/', include('api.urls')),
 ]
 
 # Ajouter cette condition pour servir les médias en développement
